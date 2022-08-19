c0cc3b3bb5de952c52c3cd7a73f8e7c874f4d495
Oleg Zharov <levineye@gmail.com>
2022-08-19 20:41:24 +0300

    feat: удалить лишний код и изображения

---
+++ src/images/index.ts
@@ -0,0 +1,7 @@
+import { greenList } from './green';
+import { redList } from './red';
+import { yellowList } from './yellow';
+
+const images = [...greenList, ...redList, ...yellowList];
+
+export { images };
